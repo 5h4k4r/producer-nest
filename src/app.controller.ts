743e317c20527c8232559fd913510be634b54db7
5h4k4r <shakar.halabjai@yahoo.com>
2022-07-20 18:06:20 +0300

    Update app controller use rabbit mq service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RabbitMqService } from './rabbit-mq.service';
+import { take } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly rabbitMqService: RabbitMqService,
+  ) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const pendingOperations = Array.from(new Array(5)).map((_, index) =>
+      this.rabbitMqService.send('rabbit-mq-producer', {
+        message: this.appService.getHello() + index,
+      }),
+    );
+    Promise.all(pendingOperations);
+
+    return 'Message sent to the queue!';
   }
 }
